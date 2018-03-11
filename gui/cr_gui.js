67c57baa6e01633fac026271efaa1a642fc9bef9
joao-carloto <joao.m.carloto@gmail.com>
2018-03-11 22:42:28 +0000

    Added google analytics

--- gui/cr_gui.js
+++ gui/cr_gui.js
@@ -3,6 +3,7 @@ var enabledLocators;
 var $focusedVar;
 var tabID;
 
+// Setting up the jqWidgets
 $(document).ready(function () {
 
     tabID = chrome.devtools.inspectedWindow.tabId;
@@ -499,3 +500,14 @@ function getTimeStamp() {
         ((now.getSeconds() < 10) ? ("0" + now.getSeconds()) : (now.getSeconds())).toString()
     );
 }
+
+// Goolge analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-115470193-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
